eaebc58a3c77d07cec23cae22553fa1f50bc7d0b
Chand Pathan <chand.apsar.pathan@gmail.com>
2026-07-07 23:17:02 +0530

    Add files via upload

---
+++ Practice/program24.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    //sequence
+    printf("jay Ganesh\n");
+     printf("jay Ganesh\n");
+      printf("jay Ganesh\n");
+       printf("jay Ganesh\n");
+    return 0;
+}
---
+++ Practice/program25.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt=0;
+
+    //Iteration
+    for(iCnt=1;iCnt<=4;iCnt++)
+    {
+        printf("jay Ganesh\n");
+    }
+    return 0;
+}
+
+/*
+dry run is the procesur that we follow to execute the code without machine(on paper)
+
+int iCnt=0;
+      1       2       3
+for(iCnt=1;iCnt<=4;iCnt++)
+{
+  printf("jay ganesh\n")    4
+}
+
+1: it is loop counter initialization
+2: it is loop condition
+3: it is displacement of loop counter
+4: it is loop body
+
+Execution flow
+1->
+2->4->3
+2->4->3
+2->4->3
+2->4->3
+2
+
+
+
+*/
